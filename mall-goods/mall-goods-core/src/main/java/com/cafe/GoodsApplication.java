ec87394b214de98263242d3b3ff0d7df488f527f
zhouboyi <1144188685@qq.com>
2022-04-25 16:46:38 +0800

    修改商品模块架构

--- mall-goods/mall-goods-core/src/main/java/com/cafe/GoodsApplication.java
+++ mall-goods/mall-goods-core/src/main/java/com/cafe/GoodsApplication.java
@@ -1,18 +1,16 @@
 package com.cafe;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 
 /**
  * @Project: mall-cloud
  * @Package: com.cafe
  * @Author: zhouboyi
- * @Date: 2022/4/25 9:42
+ * @Date: 2022/4/25 14:16
  * @Description:
  */
-@SpringBootApplication
-@EnableEurekaClient
+@SpringCloudApplication
 public class GoodsApplication {
 
     public static void main(String[] args) {
