065016a864b9e1d555cbbfa6e45fb6c64dceb76b
zhouboyi <1144188685@qq.com>
2022-05-05 17:45:53 +0800

    feat(order): 新增订单模块

--- mall-order/mall-order-core/src/main/java/com/cafe/order/OrderApplication.java
+++ mall-order/mall-order-core/src/main/java/com/cafe/order/OrderApplication.java
@@ -1,21 +1,23 @@
-package com.cafe.goods;
+package com.cafe.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @Project: mall-cloud
- * @Package: com.cafe
+ * @Package: com.cafe.order
  * @Author: zhouboyi
- * @Date: 2022/4/25 14:16
+ * @Date: 2022/5/6 9:34
  * @Description:
  */
 @SpringCloudApplication
+@EnableFeignClients(basePackages = "com.cafe.goods.feign")
 @ComponentScan(basePackages = "com.cafe")
-public class GoodsApplication {
+public class OrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GoodsApplication.class, args);
+        SpringApplication.run(OrderApplication.class, args);
     }
 }
