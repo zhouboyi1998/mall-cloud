ac8563ad4e661dbba1726d11667774cfd2b11e20
zhouboyi <1144188685@qq.com>
2022-05-06 15:31:31 +0800

    feat(security): 新增Spring Security安全模块和管理员模块

---
+++ mall-security/mall-security-management/src/main/java/com/cafe/security/management/SecurityManagementApplication.java
@@ -0,0 +1,19 @@
+package com.cafe.security.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.security.management
+ * @Author: zhouboyi
+ * @Date: 2022/5/6 10:54
+ * @Description:
+ */
+@SpringCloudApplication
+public class SecurityManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityManagementApplication.class, args);
+    }
+}
