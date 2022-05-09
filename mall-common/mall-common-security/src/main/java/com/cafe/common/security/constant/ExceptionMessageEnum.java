ba7514c56bbf28833c33a7d10b5a3ae020f8071a
zhouboyi <1144188685@qq.com>
2022-05-09 17:48:50 +0800

    feat(security): 安全模块新增配置类和公共代码

---
+++ mall-common/mall-common-security/src/main/java/com/cafe/common/security/constant/ExceptionMessageEnum.java
@@ -0,0 +1,39 @@
+package com.cafe.common.security.constant;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.common.security.enumerate
+ * @Author: zhouboyi
+ * @Date: 2022/5/9 9:09
+ * @Description: 错误信息枚举
+ */
+public enum ExceptionMessageEnum {
+
+    /**
+     * 枚举值
+     */
+    USERNAME_NOTFOUND(1001, "账号不存在"),
+    ACCOUNT_DISABLED(1002, "账号不可用"),
+    ACCOUNT_LOCKED(1003, "账号被锁定"),
+    ACCOUNT_EXPIRED(1004, "账号已过期"),
+    CREDENTIALS_EXPIRED(1005, "证书已过期");
+
+    /**
+     * 错误码
+     */
+    private Integer code;
+
+    /**
+     * 错误信息
+     */
+    private String message;
+
+    private ExceptionMessageEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- mall-security/mall-security-management/src/main/java/com/cafe/security/management/SecurityManagementApplication.java
+++ mall-security/mall-security-management/src/main/java/com/cafe/security/management/SecurityManagementApplication.java
@@ -2,6 +2,7 @@ package com.cafe.security.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @Project: mall-cloud
@@ -11,6 +12,7 @@ import org.springframework.cloud.client.SpringCloudApplication;
  * @Description:
  */
 @SpringCloudApplication
+@EnableFeignClients(basePackages = "com.cafe.admin.feign")
 public class SecurityManagementApplication {
 
     public static void main(String[] args) {
