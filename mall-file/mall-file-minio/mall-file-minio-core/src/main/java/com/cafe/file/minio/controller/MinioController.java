ec62fe390257f0eec2cdc55d5da0c90bbcdd1b4e
zhouboyi <1144188685@qq.com>
2022-06-09 17:56:53 +0800

    feat(minio): 新增MinIO文件下载接口

--- mall-file/mall-file-minio/mall-file-minio-core/src/main/java/com/cafe/file/minio/controller/MinioController.java
+++ mall-file/mall-file-minio/mall-file-minio-core/src/main/java/com/cafe/file/minio/controller/MinioController.java
@@ -1,19 +1,12 @@
 package com.cafe.file.minio.controller;
 
-import com.cafe.file.minio.property.MinioProperties;
 import com.cafe.file.minio.service.MinioService;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Project: mall-cloud
@@ -33,9 +26,41 @@ public class MinioController {
         this.minioService = minioService;
     }
 
-    @PostMapping(value = "/upload")
-    public ResponseEntity<String> upload(MultipartFile file) {
-        String result = minioService.upload(file);
+    @PostMapping(value = "/upload/{bucket}")
+    public ResponseEntity<String> upload(
+        @PathVariable(value = "bucket") String bucket,
+        MultipartFile file
+    ) {
+        String result = minioService.upload(bucket, file);
         return ResponseEntity.ok(result);
+    }
+
+    @GetMapping(value = "/download/{bucket}/{fileName}")
+    public ResponseEntity<String> download(
+        @PathVariable(value = "bucket") String bucket,
+        @PathVariable(value = "fileName") String fileName,
+        HttpServletResponse httpResponse
+    ) {
+        minioService.download(bucket, fileName, httpResponse);
+        return ResponseEntity.ok("success");
+    }
+
+    @GetMapping(value = "/url/{bucket}/{fileName}")
+    public ResponseEntity<String> getFileUrl(
+        @PathVariable(value = "bucket") String bucket,
+        @PathVariable(value = "fileName") String fileName
+    ) {
+        String url = minioService.getFileUrl(bucket, fileName);
+        return ResponseEntity.ok(url);
+    }
+
+    @GetMapping(value = "/url/{bucket}/{fileName}/{expiry}")
+    public ResponseEntity<String> getFileUrl(
+        @PathVariable(value = "bucket") String bucket,
+        @PathVariable(value = "fileName") String fileName,
+        @PathVariable(value = "expiry") Integer expiry
+    ) {
+        String url = minioService.getFileUrl(bucket, fileName, expiry);
+        return ResponseEntity.ok(url);
     }
 }
--- mall-file/mall-file-minio/mall-file-minio-core/src/main/java/com/cafe/file/minio/service/impl/MinioServiceImpl.java
+++ mall-file/mall-file-minio/mall-file-minio-core/src/main/java/com/cafe/file/minio/service/impl/MinioServiceImpl.java
@@ -4,15 +4,17 @@ import com.cafe.file.minio.controller.MinioController;
 import com.cafe.file.minio.property.MinioProperties;
 import com.cafe.file.minio.service.MinioService;
 import com.cafe.file.minio.tool.MinioTool;
-import io.minio.MinioClient;
-import io.minio.PutObjectArgs;
+import io.minio.*;
+import io.minio.http.Method;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FastByteArrayOutputStream;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Project: mall-cloud
@@ -40,22 +42,102 @@ public class MinioServiceImpl implements MinioService {
     }
 
     @Override
-    public String upload(MultipartFile file) {
+    public String upload(String bucket, MultipartFile file) {
         try {
             // 生成文件名
             String fileName = MinioTool.generateFileName();
+            // 构造参数
             PutObjectArgs args = PutObjectArgs
                 .builder()
-                .bucket(minioProperties.getBucket())
+                .bucket(bucket)
                 .object(fileName)
                 .contentType(file.getContentType())
                 .stream(file.getInputStream(), file.getSize(), -1)
                 .build();
-
+            // 上传文件
             minioClient.putObject(args);
-            return fileName;
+            // 获取文件存储路径
+            return "/" + bucket + "/" + fileName;
         } catch (Exception e) {
             LOGGER.error("MinioServiceImpl.upload() failed to upload file: {}", e.getMessage());
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    public void download(String bucket, String fileName, HttpServletResponse httpResponse) {
+        try {
+            // 构造参数
+            GetObjectArgs args = GetObjectArgs
+                .builder()
+                .bucket(bucket)
+                .object(fileName)
+                .build();
+            GetObjectResponse minioResponse = minioClient.getObject(args);
+
+            // 用于临时缓冲的 byte 数组
+            byte[] buffer = new byte[1024];
+            // 用于临时存储读取的字节长度
+            int length = 0;
+
+            // 从 MinIO 服务器读取文件字节流
+            FastByteArrayOutputStream byteArrayOS = new FastByteArrayOutputStream();
+            while ((length = minioResponse.read(buffer)) != -1) {
+                byteArrayOS.write(buffer, 0, length);
+            }
+            byteArrayOS.flush();
+
+            // 转换成完整的文件字节流
+            byte[] bytes = byteArrayOS.toByteArray();
+
+            // 配置 HTTP Response
+            httpResponse.setCharacterEncoding(minioProperties.getCharacterEncoding());
+            httpResponse.setContentType(minioProperties.getContentType());
+            httpResponse.setHeader(minioProperties.getHeaderKey(), minioProperties.getHeaderValuePrefix() + fileName);
+
+            // 写入 HTTP Response 中
+            ServletOutputStream httpOS = httpResponse.getOutputStream();
+            httpOS.write(bytes);
+            httpOS.flush();
+
+        } catch (Exception e) {
+            LOGGER.error("MinioServiceImpl.download() failed to download file: {}", e.getMessage());
+        }
+    }
+
+    @Override
+    public String getFileUrl(String bucket, String fileName) {
+        try {
+            // 构造参数
+            GetPresignedObjectUrlArgs args = GetPresignedObjectUrlArgs
+                .builder()
+                .bucket(bucket)
+                .object(fileName)
+                .method(Method.GET)
+                .build();
+            // 获取文件外链 URL
+            return minioClient.getPresignedObjectUrl(args);
+        } catch (Exception e) {
+            LOGGER.error("MinioServiceImpl.getUrl() failed to get file url: {}", e.getMessage());
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    public String getFileUrl(String bucket, String fileName, Integer expiry) {
+        try {
+            // 构造参数
+            GetPresignedObjectUrlArgs args = GetPresignedObjectUrlArgs
+                .builder()
+                .expiry(expiry)
+                .bucket(bucket)
+                .object(fileName)
+                .method(Method.GET)
+                .build();
+            // 获取文件外链 URL
+            return minioClient.getPresignedObjectUrl(args);
+        } catch (Exception e) {
+            LOGGER.error("MinioServiceImpl.getUrl() failed to get file url: {}", e.getMessage());
             return e.getMessage();
         }
     }
