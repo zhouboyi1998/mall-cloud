82da76f1d8e1a50bf89be4bb3dd6b4ef2f85df85
zhouboyi <1144188685@qq.com>
2022-07-12 23:55:20 +0800

    启动类添加Swagger注解

--- mall-order/mall-order-core/src/main/java/com/cafe/order/OrderApplication.java
+++ mall-order/mall-order-core/src/main/java/com/cafe/order/OrderApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * @Project: mall-cloud
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @Description: 订单模块启动类
  */
 @SpringCloudApplication
+@EnableSwagger2WebMvc
 @EnableFeignClients(basePackages = "com.cafe.goods.feign")
 @ComponentScan(basePackages = "com.cafe")
 public class OrderApplication {
