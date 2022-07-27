8a32ab78ec89b82ba9ead86e9397ae672366e6d5
zhouboyi <1144188685@qq.com>
2022-07-27 11:38:24 +0800

    feat(search): 新增ElasticSearch搜索引擎模块

---
+++ mall-search/mall-search-elasticsearch/mall-search-elasticsearch-core/src/main/java/com/cafe/search/elasticsearch/service/impl/ElasticSearchServiceImpl.java
@@ -0,0 +1,35 @@
+package com.cafe.search.elasticsearch.service.impl;
+
+import com.cafe.search.elasticsearch.service.ElasticSearchService;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.search.elasticsearch.service.impl
+ * @Author: zhouboyi
+ * @Date: 2022/7/27 11:19
+ * @Description:
+ */
+@Service
+public class ElasticSearchServiceImpl implements ElasticSearchService {
+
+    private RestHighLevelClient restHighLevelClient;
+
+    public ElasticSearchServiceImpl(RestHighLevelClient restHighLevelClient) {
+        this.restHighLevelClient = restHighLevelClient;
+    }
+
+    @Override
+    public SearchResponse info() throws IOException {
+        SearchRequest searchRequest = new SearchRequest().source(new SearchSourceBuilder());
+        SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        return searchResponse;
+    }
+}
