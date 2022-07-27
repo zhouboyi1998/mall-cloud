f64491693f26fbd0e38146b62b289deb69b32a6f
zhouboyi <1144188685@qq.com>
2022-07-27 16:10:00 +0800

    feat(elasticsearch): 新增ElasticSearch索引相关接口

--- mall-search/mall-search-elasticsearch/mall-search-elasticsearch-core/src/main/java/com/cafe/search/elasticsearch/service/impl/ElasticSearchServiceImpl.java
+++ mall-search/mall-search-elasticsearch/mall-search-elasticsearch-core/src/main/java/com/cafe/search/elasticsearch/service/impl/ElasticSearchServiceImpl.java
@@ -1,11 +1,17 @@
 package com.cafe.search.elasticsearch.service.impl;
 
 import com.cafe.search.elasticsearch.service.ElasticSearchService;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -22,6 +28,7 @@ public class ElasticSearchServiceImpl implements ElasticSearchService {
 
     private RestHighLevelClient restHighLevelClient;
 
+    @Autowired
     public ElasticSearchServiceImpl(RestHighLevelClient restHighLevelClient) {
         this.restHighLevelClient = restHighLevelClient;
     }
@@ -32,5 +39,28 @@ public class ElasticSearchServiceImpl implements ElasticSearchService {
         SearchRequest searchRequest = new SearchRequest().source(searchSourceBuilder);
         SearchResponse searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
         return searchResponse;
+    }
+
+    @Override
+    public Boolean existsIndex(String name) throws IOException {
+        GetIndexRequest getIndexRequest = new GetIndexRequest(name);
+        Boolean exists = restHighLevelClient.indices().exists(getIndexRequest, RequestOptions.DEFAULT);
+        return exists;
+    }
+
+    @Override
+    public CreateIndexResponse createIndex(String name) throws IOException {
+        CreateIndexRequest createIndexRequest = new CreateIndexRequest(name);
+        CreateIndexResponse createIndexResponse
+            = restHighLevelClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+        return createIndexResponse;
+    }
+
+    @Override
+    public AcknowledgedResponse deleteIndex(String name) throws IOException {
+        DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest(name);
+        AcknowledgedResponse acknowledgedResponse
+            = restHighLevelClient.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT);
+        return acknowledgedResponse;
     }
 }
