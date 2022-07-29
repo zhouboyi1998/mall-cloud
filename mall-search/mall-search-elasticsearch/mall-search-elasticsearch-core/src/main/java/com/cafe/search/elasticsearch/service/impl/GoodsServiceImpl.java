fc77e41ca960da4c35914014011c9db5cf772fe1
zhouboyi <1144188685@qq.com>
2022-07-29 14:43:58 +0800

    feat(elasticsearch): ElasticSearch模块新增单条插入、单条更新、单条删除接口

--- mall-search/mall-search-elasticsearch/mall-search-elasticsearch-core/src/main/java/com/cafe/search/elasticsearch/service/impl/GoodsServiceImpl.java
+++ mall-search/mall-search-elasticsearch/mall-search-elasticsearch-core/src/main/java/com/cafe/search/elasticsearch/service/impl/GoodsServiceImpl.java
@@ -9,10 +9,15 @@ import com.cafe.search.elasticsearch.service.GoodsService;
 import org.elasticsearch.action.bulk.BulkRequest;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.unit.TimeValue;
@@ -51,6 +56,48 @@ public class GoodsServiceImpl implements GoodsService {
     }
 
     @Override
+    public GetResponse one(String _id) throws IOException {
+        // 组装查询请求
+        GetRequest getRequest = new GetRequest(ElasticSearchConstant.GOODS_INDEX, _id);
+        // 查询数据
+        GetResponse getResponse = restHighLevelClient.get(getRequest, RequestOptions.DEFAULT);
+        return getResponse;
+    }
+
+    @Override
+    public IndexResponse insert(SkuElasticSearchDTO dto) throws IOException {
+        // 组装插入请求
+        IndexRequest indexRequest = new IndexRequest(ElasticSearchConstant.GOODS_INDEX)
+            .timeout(TimeValue.timeValueSeconds(10))
+            .id(dto.getId().toString())
+            .source(JSONUtil.toJsonStr(dto), XContentType.JSON);
+        // 插入数据
+        IndexResponse indexResponse = restHighLevelClient.index(indexRequest, RequestOptions.DEFAULT);
+        return indexResponse;
+    }
+
+    @Override
+    public UpdateResponse update(SkuElasticSearchDTO dto) throws IOException {
+        // 组装更新请求
+        UpdateRequest updateRequest = new UpdateRequest(ElasticSearchConstant.GOODS_INDEX, dto.getId().toString())
+            .timeout(TimeValue.timeValueSeconds(10))
+            .doc(JSONUtil.toJsonStr(dto), XContentType.JSON);
+        // 更新数据
+        UpdateResponse updateResponse = restHighLevelClient.update(updateRequest, RequestOptions.DEFAULT);
+        return updateResponse;
+    }
+
+    @Override
+    public DeleteResponse delete(String _id) throws IOException {
+        // 组装删除请求
+        DeleteRequest deleteRequest = new DeleteRequest(ElasticSearchConstant.GOODS_INDEX, _id)
+            .timeout(TimeValue.timeValueSeconds(10));
+        // 删除数据
+        DeleteResponse deleteResponse = restHighLevelClient.delete(deleteRequest, RequestOptions.DEFAULT);
+        return deleteResponse;
+    }
+
+    @Override
     public BulkResponse insertBatch(List<SkuElasticSearchDTO> dtoList) throws IOException {
         // 组装批量插入请求
         BulkRequest bulkRequest = new BulkRequest().timeout(TimeValue.timeValueSeconds(60));
@@ -81,11 +128,11 @@ public class GoodsServiceImpl implements GoodsService {
     }
 
     @Override
-    public BulkResponse deleteBatch(List<String> esIds) throws IOException {
+    public BulkResponse deleteBatch(List<String> _ids) throws IOException {
         // 组装批量删除请求
         BulkRequest bulkRequest = new BulkRequest().timeout(TimeValue.timeValueSeconds(60));
-        for (String esId : esIds) {
-            DeleteRequest deleteRequest = new DeleteRequest(ElasticSearchConstant.GOODS_INDEX, esId);
+        for (String _id : _ids) {
+            DeleteRequest deleteRequest = new DeleteRequest(ElasticSearchConstant.GOODS_INDEX, _id);
             bulkRequest.add(deleteRequest);
         }
         // 批量删除数据
@@ -94,7 +141,7 @@ public class GoodsServiceImpl implements GoodsService {
     }
 
     @Override
-    public SearchResponse search(Integer current, Integer size, String keyword, String sort, String rule) throws IOException {
+    public SearchResponse page(Integer current, Integer size, String keyword, String sort, String rule) throws IOException {
         // 组装搜索条件
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder()
             // 分页条件
