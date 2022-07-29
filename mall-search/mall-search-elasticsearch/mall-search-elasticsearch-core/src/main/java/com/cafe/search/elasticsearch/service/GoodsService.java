51029254b9b1a88571fb362077a680a09ab25abd
zhouboyi <1144188685@qq.com>
2022-07-29 14:43:58 +0800

    feat(elasticsearch): ElasticSearch模块新增单条插入、单条更新、单条删除接口

--- mall-search/mall-search-elasticsearch/mall-search-elasticsearch-core/src/main/java/com/cafe/search/elasticsearch/service/GoodsService.java
+++ mall-search/mall-search-elasticsearch/mall-search-elasticsearch-core/src/main/java/com/cafe/search/elasticsearch/service/GoodsService.java
@@ -2,7 +2,11 @@ package com.cafe.search.elasticsearch.service;
 
 import com.cafe.goods.dto.SkuElasticSearchDTO;
 import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.update.UpdateResponse;
 
 import java.io.IOException;
 import java.util.List;
@@ -15,6 +19,42 @@ import java.util.List;
  * @Description:
  */
 public interface GoodsService {
+
+    /**
+     * 获取商品
+     *
+     * @param _id
+     * @return
+     * @throws IOException
+     */
+    GetResponse one(String _id) throws IOException;
+
+    /**
+     * 插入商品
+     *
+     * @param dto
+     * @return
+     * @throws IOException
+     */
+    IndexResponse insert(SkuElasticSearchDTO dto) throws IOException;
+
+    /**
+     * 更新商品
+     *
+     * @param dto
+     * @return
+     * @throws IOException
+     */
+    UpdateResponse update(SkuElasticSearchDTO dto) throws IOException;
+
+    /**
+     * 删除商品
+     *
+     * @param _id
+     * @return
+     * @throws IOException
+     */
+    DeleteResponse delete(String _id) throws IOException;
 
     /**
      * 批量插入商品
@@ -37,11 +77,11 @@ public interface GoodsService {
     /**
      * 批量删除商品
      *
-     * @param esIds
+     * @param _ids
      * @return
      * @throws IOException
      */
-    BulkResponse deleteBatch(List<String> esIds) throws IOException;
+    BulkResponse deleteBatch(List<String> _ids) throws IOException;
 
     /**
      * 搜索商品
@@ -54,7 +94,7 @@ public interface GoodsService {
      * @return
      * @throws IOException
      */
-    SearchResponse search(Integer current, Integer size, String keyword, String sort, String rule) throws IOException;
+    SearchResponse page(Integer current, Integer size, String keyword, String sort, String rule) throws IOException;
 
     /**
      * 批量导入商品
