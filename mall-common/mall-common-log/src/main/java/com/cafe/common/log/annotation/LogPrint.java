24dccbe7cb9faadaee0c8291c59eda28d5551b7a
zhouboyi <1144188685@qq.com>
2022-09-16 15:09:10 +0800

    feat(log): 新增自定义日志打印注解

---
+++ mall-common/mall-common-log/src/main/java/com/cafe/common/log/annotation/LogPrint.java
@@ -0,0 +1,24 @@
+package com.cafe.common.log.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.common.log.annotation
+ * @Author: zhouboyi
+ * @Date: 2022/9/16 9:19
+ * @Description: 接口日志打印注解
+ */
+// 生成 Java 文档时, 文档只会记录有 @Documented 修饰的注解
+@Documented
+// 注解保留策略, RUNTIME: 一直保留到运行阶段 (JVM 加载 .class 文件后, 注解仍然保留)
+@Retention(RetentionPolicy.RUNTIME)
+// 注解添加的位置
+@Target(ElementType.METHOD)
+public @interface LogPrint {
+    String description() default "";
+}
