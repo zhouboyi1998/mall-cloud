eff7e27aa1d147b355040d3767ce81cdb203becb
zhouboyi <1144188685@qq.com>
2022-09-29 16:20:15 +0800

    feat(exception): 新增全局异常处理器

---
+++ mall-common/mall-common-constant/src/main/java/com/cafe/common/enumeration/HttpStatusCodeEnum.java
@@ -0,0 +1,64 @@
+package com.cafe.common.enumeration;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.common.enumeration
+ * @Author: zhouboyi
+ * @Date: 2022/5/9 9:09
+ * @Description: 自定义状态码枚举
+ */
+public enum HttpStatusCodeEnum {
+
+    /**
+     * 用户不存在
+     */
+    USERNAME_NOT_FOUND(4001, "Username Not Found"),
+
+    /**
+     * 账号已禁用
+     */
+    ACCOUNT_DISABLED(4002, "Account Disabled"),
+
+    /**
+     * 账号被锁定
+     */
+    ACCOUNT_LOCKED(4003, "Account Locked"),
+
+    /**
+     * 账号已过期
+     */
+    ACCOUNT_EXPIRED(4004, "Account Expired"),
+
+    /**
+     * 证书已过期
+     */
+    CREDENTIALS_EXPIRED(4005, "Credentials Expired"),
+
+    /**
+     * 商城内部服务错误
+     */
+    MALL_INTERNAL_SERVER_ERROR(5000, "Mall Internal Server Error");
+
+    /**
+     * 状态码
+     */
+    private Integer code;
+
+    /**
+     * 状态信息
+     */
+    private String message;
+
+    HttpStatusCodeEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Integer getCode() {
+        return this.code;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
