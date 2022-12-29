1bbb501d9a9747df49f2e08a9ea2ea9eb8b3184c
zhouboyi <1144188685@qq.com>
2022-12-29 17:00:27 +0800

    feat(system): 新增系统模块

---
+++ mall-system/mall-system-core/src/main/java/com/cafe/system/service/AreaService.java
@@ -0,0 +1,15 @@
+package com.cafe.system.service;
+
+import com.cafe.system.model.Area;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.system.service
+ * @Author: zhouboyi
+ * @Date: 2022-12-29
+ * @Description: 地址业务接口
+ */
+public interface AreaService extends IService<Area> {
+
+}
---
+++ mall-system/mall-system-core/src/main/java/com/cafe/system/service/impl/AreaServiceImpl.java
@@ -0,0 +1,26 @@
+package com.cafe.system.service.impl;
+
+import com.cafe.system.model.Area;
+import com.cafe.system.dao.AreaMapper;
+import com.cafe.system.service.AreaService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.system.service.impl
+ * @Author: zhouboyi
+ * @Date: 2022-12-29
+ * @Description: 地址业务实现类
+ */
+@Service
+public class AreaServiceImpl extends ServiceImpl<AreaMapper, Area> implements AreaService {
+
+    private final AreaMapper areaMapper;
+
+    @Autowired
+    public AreaServiceImpl(AreaMapper areaMapper) {
+        this.areaMapper = areaMapper;
+    }
+}
