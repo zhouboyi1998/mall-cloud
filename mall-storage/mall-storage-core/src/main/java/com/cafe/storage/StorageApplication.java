a312e60a382a6e0f123e8baff7d341a6f4d100ce
zhouboyi <1144188685@qq.com>
2022-12-29 19:39:11 +0800

    feat(storage): 新增仓库模块

---
+++ mall-storage/mall-storage-core/src/main/java/com/cafe/storage/StorageApplication.java
@@ -0,0 +1,27 @@
+package com.cafe.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.storage
+ * @Author: zhouboyi
+ * @Date: 2022/12/29 19:34
+ * @Description:
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableSwagger2WebMvc
+@EnableFeignClients(basePackages = "com.cafe.id.feign")
+@ComponentScan(basePackages = "com.cafe")
+public class StorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorageApplication.class, args);
+    }
+}
---
+++ mall-storage/mall-storage-core/src/main/java/com/cafe/storage/service/impl/StorageServiceImpl.java
@@ -0,0 +1,26 @@
+package com.cafe.storage.service.impl;
+
+import com.cafe.storage.model.Storage;
+import com.cafe.storage.dao.StorageMapper;
+import com.cafe.storage.service.StorageService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.storage.service.impl
+ * @Author: zhouboyi
+ * @Date: 2022-12-29
+ * @Description: 仓库业务实现类
+ */
+@Service
+public class StorageServiceImpl extends ServiceImpl<StorageMapper, Storage> implements StorageService {
+
+    private final StorageMapper storageMapper;
+
+    @Autowired
+    public StorageServiceImpl(StorageMapper storageMapper) {
+        this.storageMapper = storageMapper;
+    }
+}
