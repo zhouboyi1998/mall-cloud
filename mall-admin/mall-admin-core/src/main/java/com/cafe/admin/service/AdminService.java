1592e29cbc55dd9afdefaa105353b73230c09f73
zhouboyi <1144188685@qq.com>
2022-12-29 17:19:29 +0800

    feat(admin): 生成管理员模块代码

---
+++ mall-admin/mall-admin-core/src/main/java/com/cafe/admin/service/AdminService.java
@@ -0,0 +1,15 @@
+package com.cafe.admin.service;
+
+import com.cafe.admin.model.Admin;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.admin.service
+ * @Author: zhouboyi
+ * @Date: 2022-12-29
+ * @Description: 管理员业务接口
+ */
+public interface AdminService extends IService<Admin> {
+
+}
