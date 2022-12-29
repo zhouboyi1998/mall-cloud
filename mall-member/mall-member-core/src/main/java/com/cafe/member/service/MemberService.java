34ca20fcbfc0d47d0fdc4390c48478675c802fdd
zhouboyi <1144188685@qq.com>
2022-12-29 17:27:37 +0800

    feat(member): 生成会员模块代码

---
+++ mall-member/mall-member-core/src/main/java/com/cafe/member/service/MemberService.java
@@ -0,0 +1,15 @@
+package com.cafe.member.service;
+
+import com.cafe.member.model.Member;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.member.service
+ * @Author: zhouboyi
+ * @Date: 2022-12-29
+ * @Description: 会员业务接口
+ */
+public interface MemberService extends IService<Member> {
+
+}
