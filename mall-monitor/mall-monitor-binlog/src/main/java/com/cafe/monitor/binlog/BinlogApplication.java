8529adfab02c64a5762241b38aade23b4368a330
zhouboyi <1144188685@qq.com>
2023-01-16 19:07:11 +0800

    fix(monitor): 数据库监听器模块开启FeignClient扫描

--- mall-monitor/mall-monitor-binlog/src/main/java/com/cafe/monitor/binlog/BinlogApplication.java
+++ mall-monitor/mall-monitor-binlog/src/main/java/com/cafe/monitor/binlog/BinlogApplication.java
@@ -3,6 +3,7 @@ package com.cafe.monitor.binlog;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @Description: MySQL Binlog 数据库监听模块启动类
  */
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.cafe.id.feign")
 @ComponentScan(basePackages = "com.cafe")
 public class BinlogApplication {
 
