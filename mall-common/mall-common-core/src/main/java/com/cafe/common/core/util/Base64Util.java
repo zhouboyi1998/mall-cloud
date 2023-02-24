1b04bcc47bdd6569011fdd0f889535de6f3f3b01
zhouboyi <1144188685@qq.com>
2023-02-25 02:26:02 +0800

    feat(base64): 新增Base64编码工具类

---
+++ mall-common/mall-common-core/src/main/java/com/cafe/common/core/util/Base64Util.java
@@ -0,0 +1,40 @@
+package com.cafe.common.core.util;
+
+import com.cafe.common.enumeration.MediaFormatEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.common.core.util
+ * @Author: zhouboyi
+ * @Date: 2023/2/25 0:20
+ * @Description: Base64 编码工具类
+ */
+public class Base64Util {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Base64Util.class);
+
+    /**
+     * BufferedImage 格式图片转换成 Base64 编码
+     *
+     * @param bufferedImage
+     * @return
+     */
+    public static String encode(BufferedImage bufferedImage, MediaFormatEnum mediaFormat) {
+        // 将图片转换成 Base64 编码
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            ImageIO.write(bufferedImage, mediaFormat.getFormat(), outputStream);
+        } catch (IOException e) {
+            LOGGER.error("Base64Util.encode(): could not convert image to base64 -> {}", e.getMessage());
+        }
+        return mediaFormat.getBase64Prefix() + Base64.getEncoder().encodeToString(outputStream.toByteArray());
+    }
+}
