2e51f1800b106a7c1555ef447023f1ae7a50bf0e
zhouboyi <1144188685@qq.com>
2023-02-27 10:52:37 +0800

    feat(security): 新增生成Hutool Captcha图片验证码功能

---
+++ mall-common/mall-common-constant/src/main/java/com/cafe/common/constant/CaptchaConstant.java
@@ -0,0 +1,26 @@
+package com.cafe.common.constant;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.common.constant
+ * @Author: zhouboyi
+ * @Date: 2023/2/27 10:30
+ * @Description: 图片验证码相关常量
+ */
+public class CaptchaConstant {
+
+    /**
+     * 图片宽度
+     */
+    public static final Integer WIDTH = 130;
+
+    /**
+     * 图片高度
+     */
+    public static final Integer HEIGHT = 48;
+
+    /**
+     * 文本长度
+     */
+    public static final Integer LENGTH = 4;
+}
