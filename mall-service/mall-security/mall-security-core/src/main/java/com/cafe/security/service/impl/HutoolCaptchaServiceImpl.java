c7102254772a3ccfb8207cf83ab1705c28a16db4
zhouboyi <1144188685@qq.com>
2023-02-27 10:52:37 +0800

    feat(security): 新增生成Hutool Captcha图片验证码功能

--- mall-service/mall-security/mall-security-core/src/main/java/com/cafe/security/service/impl/HutoolCaptchaServiceImpl.java
+++ mall-service/mall-security/mall-security-core/src/main/java/com/cafe/security/service/impl/HutoolCaptchaServiceImpl.java
@@ -1,12 +1,14 @@
 package com.cafe.security.service.impl;
 
+import cn.hutool.captcha.CaptchaUtil;
+import cn.hutool.captcha.LineCaptcha;
 import com.cafe.common.constant.CaptchaConstant;
 import com.cafe.common.constant.NumberConstant;
 import com.cafe.common.constant.RedisConstant;
+import com.cafe.common.enumeration.MediaFormatEnum;
 import com.cafe.id.feign.IDFeign;
 import com.cafe.security.model.Captcha;
 import com.cafe.security.service.CaptchaService;
-import com.wf.captcha.SpecCaptcha;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -17,18 +19,18 @@ import java.util.concurrent.TimeUnit;
  * @Project: mall-cloud
  * @Package: com.cafe.security.service.impl
  * @Author: zhouboyi
- * @Date: 2023/2/24 23:42
- * @Description: Easy Captcha 图片验证码业务实现类
+ * @Date: 2023/2/27 9:56
+ * @Description: Hutool Captcha 图片验证码业务实现类
  */
-@Service(value = "EasyCaptchaServiceImpl")
-public class EasyCaptchaServiceImpl implements CaptchaService {
+@Service(value = "HutoolCaptchaServiceImpl")
+public class HutoolCaptchaServiceImpl implements CaptchaService {
 
     private final IDFeign idFeign;
 
     private final RedisTemplate<String, Object> redisTemplate;
 
     @Autowired
-    public EasyCaptchaServiceImpl(IDFeign idFeign, RedisTemplate<String, Object> redisTemplate) {
+    public HutoolCaptchaServiceImpl(IDFeign idFeign, RedisTemplate<String, Object> redisTemplate) {
         this.idFeign = idFeign;
         this.redisTemplate = redisTemplate;
     }
@@ -36,9 +38,9 @@ public class EasyCaptchaServiceImpl implements CaptchaService {
     @Override
     public Captcha one() {
         // 生成图片验证码
-        SpecCaptcha specCaptcha = new SpecCaptcha(CaptchaConstant.WIDTH, CaptchaConstant.HEIGHT, CaptchaConstant.LENGTH);
+        LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(CaptchaConstant.WIDTH, CaptchaConstant.HEIGHT, CaptchaConstant.LENGTH, NumberConstant.FIFTY);
         // 获取图片验证码文本
-        String code = specCaptcha.text().toUpperCase();
+        String code = lineCaptcha.getCode().toUpperCase();
 
         // 生成图片验证码唯一标识
         Long key = idFeign.nextId().getBody();
@@ -47,6 +49,6 @@ public class EasyCaptchaServiceImpl implements CaptchaService {
         redisTemplate.opsForValue().set(RedisConstant.CAPTCHA_PREFIX + key, code, NumberConstant.SIXTY, TimeUnit.SECONDS);
 
         // 返回图片验证码
-        return new Captcha().setKey(key).setImage(specCaptcha.toBase64());
+        return new Captcha().setKey(key).setImage(MediaFormatEnum.PNG.getBase64Prefix() + lineCaptcha.getImageBase64());
     }
 }
