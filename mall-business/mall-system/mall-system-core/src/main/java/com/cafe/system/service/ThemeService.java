b86cc19c20b984d9984350bc2fd491ca4bf9af47
zhouboyi <1144188685@qq.com>
2023-05-16 10:42:46 +0800

    feat(system): 生成主题功能代码

---
+++ mall-business/mall-system/mall-system-core/src/main/java/com/cafe/system/service/ThemeService.java
@@ -0,0 +1,15 @@
+package com.cafe.system.service;
+
+import com.cafe.system.model.Theme;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.system.service
+ * @Author: zhouboyi
+ * @Date: 2023-05-16
+ * @Description: 主题业务接口
+ */
+public interface ThemeService extends IService<Theme> {
+
+}
