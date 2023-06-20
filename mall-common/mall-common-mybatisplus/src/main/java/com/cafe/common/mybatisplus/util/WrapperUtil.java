f4b5cf7facc83c74f37ea781ec3518b9ad922e6a
zhouboyi <1144188685@qq.com>
2023-06-20 17:46:43 +0800

    feat(lang): 新增数据类型公共包，添加时间区间类型

--- mall-common/mall-common-mybatisplus/src/main/java/com/cafe/common/mybatisplus/util/WrapperUtil.java
+++ mall-common/mall-common-mybatisplus/src/main/java/com/cafe/common/mybatisplus/util/WrapperUtil.java
@@ -5,8 +5,9 @@ import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.AbstractWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.cafe.common.constant.app.FieldConstant;
+import com.cafe.common.constant.pool.StringConstant;
+import com.cafe.common.lang.date.AbstractPeriod;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -48,6 +49,8 @@ public class WrapperUtil {
      * @param model
      * @param wrapper
      * @param <T>
+     * @param <R>
+     * @param <Children>
      * @return
      */
     public static <T, R, Children extends AbstractWrapper<T, R, Children>> AbstractWrapper<T, R, Children> createWrapper(T model, AbstractWrapper<T, R, Children> wrapper) {
@@ -65,13 +68,28 @@ public class WrapperUtil {
             Object fieldValue = ReflectUtil.getFieldValue(model, field);
             // 如果属性值不为空, 加入 Wrapper 条件中
             if (ObjectUtil.isNotEmpty(fieldValue)) {
-                // 获取属性名
-                String fieldName = StrUtil.toUnderlineCase(field.getName());
-                // 如果属性是 String 类型, 使用 like 条件, 否则使用 eq 条件
+                // 将驼峰格式的属性名转换为下划线格式的字段名
+                String column = StrUtil.toUnderlineCase(field.getName());
                 if (fieldValue instanceof String) {
-                    wrapper.like((R) fieldName, fieldValue);
+                    // 字符串类型使用 like 条件
+                    wrapper.like((R) column, fieldValue);
+                } else if (fieldValue instanceof AbstractPeriod) {
+                    // 转换字段名
+                    column = column.replace(StringConstant.PERIOD, StringConstant.TIME);
+                    // 获取时间区间
+                    AbstractPeriod period = (AbstractPeriod) fieldValue;
+                    Object start = period.getStart();
+                    Object end = period.getEnd();
+                    // 时间区间类型使用 gt / lt 条件
+                    if (ObjectUtil.isNotEmpty(start)) {
+                        wrapper.gt((R) column, start);
+                    }
+                    if (ObjectUtil.isNotEmpty(end)) {
+                        wrapper.lt((R) column, end);
+                    }
                 } else {
-                    wrapper.eq((R) fieldName, fieldValue);
+                    // 其它类型使用 eq 条件
+                    wrapper.eq((R) column, fieldValue);
                 }
             }
         }
