dc499ab74bc6fc6b9d8d5e0a40150ec1afd18960
zhouboyi <1144188685@qq.com>
2023-06-30 10:48:56 +0800

    perf(log): 优化日志打印切面中请求参数的打印格式

--- mall-common/mall-common-log/src/main/java/com/cafe/common/log/aspect/LogPrintAspect.java
+++ mall-common/mall-common-log/src/main/java/com/cafe/common/log/aspect/LogPrintAspect.java
@@ -1,8 +1,8 @@
 package com.cafe.common.log.aspect;
 
 import cn.hutool.json.JSONUtil;
-import com.cafe.common.constant.app.FieldConstant;
 import com.cafe.common.constant.app.AppConstant;
+import com.cafe.common.constant.app.FieldConstant;
 import com.cafe.common.constant.pool.NumberConstant;
 import com.cafe.common.constant.pool.StringConstant;
 import com.cafe.common.log.annotation.LogPrint;
@@ -12,6 +12,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
@@ -20,10 +21,8 @@ import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
@@ -41,7 +40,7 @@ import java.util.stream.Collectors;
  */
 @Aspect
 @Component
-@Profile({AppConstant.DEV, AppConstant.TEST})
+@Profile(value = {AppConstant.DEV, AppConstant.TEST})
 public class LogPrintAspect {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LogPrintAspect.class);
@@ -49,7 +48,7 @@ public class LogPrintAspect {
     /**
      * 配置 @LogPrint 注解为切入点
      */
-    @Pointcut("@annotation(com.cafe.common.log.annotation.LogPrint)")
+    @Pointcut(value = "@annotation(com.cafe.common.log.annotation.LogPrint)")
     public void logPrint() {
 
     }
@@ -61,7 +60,7 @@ public class LogPrintAspect {
      * @return
      * @throws Throwable
      */
-    @Around("logPrint()")
+    @Around(value = "logPrint()")
     public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         // 为每一个请求生成 UUID 格式的 Request ID
         String requestId = UUID.randomUUID().toString();
@@ -89,7 +88,7 @@ public class LogPrintAspect {
      * @param joinPoint 连接点
      * @throws Throwable
      */
-    @Before("logPrint()")
+    @Before(value = "logPrint()")
     public void doBefore(JoinPoint joinPoint) throws Throwable {
         // 获取 HTTP 请求相关信息
         HttpServletRequest request = Optional.ofNullable(RequestContextHolder.getRequestAttributes())
@@ -99,7 +98,7 @@ public class LogPrintAspect {
         // 请求来源 IP
         LOGGER.info("Source IP        : {}", request.getRemoteAddr());
         // 描述信息
-        LOGGER.info("Description      : {}", getLogPrintDescription(joinPoint));
+        LOGGER.info("Description      : {}", description(joinPoint));
         // HTTP 请求 URL
         LOGGER.info("Request URL      : {}", request.getRequestURL());
         // HTTP 请求类型
@@ -109,38 +108,45 @@ public class LogPrintAspect {
         // 执行方法
         LOGGER.info("Method           : {}", joinPoint.getSignature().getName());
         // 请求参数
-        LOGGER.info("Request Argument : {}", getRequestArguments(joinPoint));
+        LOGGER.info("Request Argument : {}", argument(joinPoint));
     }
 
     /**
-     * 获取请求入参
+     * 获取请求参数
      *
      * @param joinPoint 连接点
      * @return
      */
-    private String getRequestArguments(JoinPoint joinPoint) {
-        // 存储请求入参
-        StringBuilder arguments = new StringBuilder();
-        // 获取所有入参, 去除 HTTP 相关参数、文件相关参数
-        List<Object> args = Arrays.stream(joinPoint.getArgs())
-            .filter(arg -> !(arg instanceof HttpServletRequest) && !(arg instanceof HttpServletResponse) &&
-                !(arg instanceof MultipartFile) && !(arg instanceof MultipartFile[]))
-            .collect(Collectors.toList());
-        // 遍历所有入参, 将参数转换为字符串, 使用 & 符号分隔
-        for (Object arg : args) {
-            // 直接拼接基本数据类型的参数, 使用 JSON 工具将其它类型的参数转换为字符串再拼接
-            if (arg instanceof Number || arg instanceof Character || arg instanceof Boolean) {
-                arguments.append(arg).append(StringConstant.AMPERSAND);
+    private String argument(JoinPoint joinPoint) {
+        // 存储请求参数
+        StringBuilder args = new StringBuilder(StringConstant.LEFT_BRACE);
+
+        // 获取参数名列表
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        List<String> keyList = Arrays.stream(signature.getParameterNames()).collect(Collectors.toList());
+        // 获取参数值列表
+        List<Object> valueList = Arrays.stream(joinPoint.getArgs()).collect(Collectors.toList());
+
+        // 遍历组装成 JSON 格式
+        for (int i = 0; i < valueList.size(); i++) {
+            args.append(StringConstant.DOUBLE_QUOTATION_MARK).append(keyList.get(i))
+                .append(StringConstant.DOUBLE_QUOTATION_MARK).append(StringConstant.COLON);
+            Object value = valueList.get(i);
+            if (value instanceof Number || value instanceof Character || value instanceof Boolean || value instanceof CharSequence) {
+                // 基本类型、字符串类型的参数直接拼接
+                args.append(StringConstant.DOUBLE_QUOTATION_MARK).append(value)
+                    .append(StringConstant.DOUBLE_QUOTATION_MARK).append(StringConstant.COMMA);
             } else {
-                arguments.append(JSONUtil.toJsonStr(arg)).append(StringConstant.AMPERSAND);
+                // 引用类型的参数转换成 JSON 字符串再拼接
+                args.append(JSONUtil.toJsonStr(value)).append(StringConstant.COMMA);
             }
         }
-        // 删除最后一个 & 符号
-        Integer index = arguments.lastIndexOf(StringConstant.AMPERSAND);
+        // 删除最后一个逗号, 拼接右花括号
+        Integer index = args.lastIndexOf(StringConstant.COMMA);
         if (index > NumberConstant.MINUS_ONE) {
-            arguments.deleteCharAt(index);
+            args.deleteCharAt(index).append(StringConstant.RIGHT_BRACE);
         }
-        return arguments.toString();
+        return args.toString();
     }
 
     /**
@@ -150,7 +156,7 @@ public class LogPrintAspect {
      * @return
      * @throws ClassNotFoundException
      */
-    private String getLogPrintDescription(JoinPoint joinPoint) throws ClassNotFoundException {
+    private String description(JoinPoint joinPoint) throws ClassNotFoundException {
         // 获取目标对象的类名
         String targetClassName = joinPoint.getTarget().getClass().getName();
         // 获取目标方法名
