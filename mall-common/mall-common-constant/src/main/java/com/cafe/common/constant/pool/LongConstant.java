a6374fd92f87d3e0f02221b27f84c98c671aa4fa
zhouboyi <1144188685@qq.com>
2023-07-06 14:32:12 +0800

    feat(cache): 新增自定义接口返回结果缓存注解

---
+++ mall-common/mall-common-constant/src/main/java/com/cafe/common/constant/pool/LongConstant.java
@@ -0,0 +1,13 @@
+package com.cafe.common.constant.pool;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.common.constant.pool
+ * @Author: zhouboyi
+ * @Date: 2023/7/5 10:05
+ * @Description: Long 数字常量
+ */
+public class LongConstant {
+
+    public static final long MINUS_ONE = -1L;
+}
