ce7c3480ff70e4fa26853d29b43dd8712e2181ce
zhouboyi <1144188685@qq.com>
2023-07-07 15:47:51 +0800

    improve(aop): AOP工具类获取请求参数方法处理文件类型参数

--- mall-common/mall-common-util/src/main/java/com/cafe/common/util/aop/AOPUtil.java
+++ mall-common/mall-common-util/src/main/java/com/cafe/common/util/aop/AOPUtil.java
@@ -5,6 +5,7 @@ import com.cafe.common.constant.pool.IntegerConstant;
 import com.cafe.common.constant.pool.StringConstant;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Arrays;
 import java.util.List;
@@ -44,6 +45,10 @@ public class AOPUtil {
             if (value instanceof Number || value instanceof Character || value instanceof Boolean || value instanceof CharSequence) {
                 // 基本类型、字符串类型的参数直接拼接
                 args.append(StringConstant.DOUBLE_QUOTATION_MARK).append(value)
+                    .append(StringConstant.DOUBLE_QUOTATION_MARK).append(StringConstant.COMMA);
+            } else if (value instanceof MultipartFile) {
+                // 文件类型的参数, 拼接文件名称
+                args.append(StringConstant.DOUBLE_QUOTATION_MARK).append(((MultipartFile) value).getOriginalFilename())
                     .append(StringConstant.DOUBLE_QUOTATION_MARK).append(StringConstant.COMMA);
             } else {
                 // 引用类型的参数转换成 JSON 字符串再拼接
