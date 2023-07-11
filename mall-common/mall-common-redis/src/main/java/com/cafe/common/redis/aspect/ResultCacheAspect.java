41ac29ff569bd5d18f3ae49db68e006fa3e5a597
zhouboyi <1144188685@qq.com>
2023-07-11 15:04:08 +0800

    fix(cache): 返回结果缓存切面修复缓存Key的组装问题，打印捕获的异常信息

--- mall-common/mall-common-redis/src/main/java/com/cafe/common/redis/aspect/ResultCacheAspect.java
+++ mall-common/mall-common-redis/src/main/java/com/cafe/common/redis/aspect/ResultCacheAspect.java
@@ -1,6 +1,7 @@
 package com.cafe.common.redis.aspect;
 
 import com.cafe.common.constant.app.AppConstant;
+import com.cafe.common.constant.pool.IntegerConstant;
 import com.cafe.common.constant.pool.StringConstant;
 import com.cafe.common.constant.redis.RedisConstant;
 import com.cafe.common.redis.annotation.ResultCache;
@@ -10,6 +11,8 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.AnnotationUtils;
@@ -20,6 +23,9 @@ import org.springframework.util.Assert;
 import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Project: mall-cloud
@@ -33,6 +39,8 @@ import java.lang.reflect.Method;
 @Component
 @Profile(value = {AppConstant.DEV, AppConstant.TEST, AppConstant.DOCKER})
 public class ResultCacheAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResultCacheAspect.class);
 
     private final RedisTemplate<String, Object> redisTemplate;
 
@@ -49,36 +57,44 @@ public class ResultCacheAspect {
     }
 
     @Around(value = "resultCache()")
-    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) {
+    public Object doAround(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         // 获取目标签名, 转换成方法签名
         MethodSignature signature = (MethodSignature) proceedingJoinPoint.getSignature();
         // 获取目标类名
         String className = proceedingJoinPoint.getTarget().getClass().getName();
         // 获取目标方法
         Method method = signature.getMethod();
+        // 获取目标方法名
+        String methodName = method.getName();
         // 获取注解
         ResultCache resultCache = AnnotationUtils.getAnnotation(method, ResultCache.class);
         Assert.notNull(resultCache, "Unable to get @ResultCache annotation!");
+        // 获取参数名列表
+        List<String> keyList = Arrays.stream(signature.getParameterNames()).collect(Collectors.toList());
+        // 获取参数值列表
+        List<Object> valueList = Arrays.stream(proceedingJoinPoint.getArgs()).collect(Collectors.toList());
 
         // 组装 Redis 缓存的 Key
         StringBuilder key = new StringBuilder()
             .append(RedisConstant.RESULT_PREFIX)
-            .append(StringUtils.hasText(resultCache.name()) ? resultCache.name() : className + StringConstant.POINT + method.getName())
+            .append(StringUtils.hasText(resultCache.name()) ? resultCache.name() : className + StringConstant.POINT + methodName)
             .append(StringConstant.COLON)
-            .append(StringUtils.hasText(resultCache.key()) ? resultCache.key() : AOPUtil.argument(proceedingJoinPoint).hashCode());
+            .append(StringUtils.hasText(resultCache.key()) ? valueList.get(keyList.indexOf(resultCache.key())) : AOPUtil.argument(proceedingJoinPoint).hashCode());
 
         try {
             // 进入连接点
             Object result = proceedingJoinPoint.proceed();
             // 方法执行正常, 将返回值存入 Redis 缓存中
             long timeout = resultCache.timeout();
-            if (timeout < 0) {
+            if (timeout < IntegerConstant.ZERO) {
                 redisTemplate.opsForValue().set(key.toString(), result);
             } else {
                 redisTemplate.opsForValue().set(key.toString(), result, timeout, resultCache.unit());
             }
             return result;
-        } catch (Throwable t) {
+        } catch (Exception e) {
+            // 打印异常信息
+            LOGGER.error("ResultCacheAspect.doAround(): class -> {}, method -> {}, message -> {}", className, methodName, e.getMessage());
             // 方法执行异常, 返回缓存在 Redis 中的上一次执行结果
             return redisTemplate.opsForValue().get(key.toString());
         }
