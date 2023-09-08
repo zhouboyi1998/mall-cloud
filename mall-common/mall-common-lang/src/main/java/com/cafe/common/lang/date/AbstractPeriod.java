5d93f68edcb33caa1b41289720b3d62df7337a64
zhouboyi <1144188685@qq.com>
2023-09-08 09:43:03 +0800

    improvement(period): 时间区间类型添加contentEquals方法

--- mall-common/mall-common-lang/src/main/java/com/cafe/common/lang/date/AbstractPeriod.java
+++ mall-common/mall-common-lang/src/main/java/com/cafe/common/lang/date/AbstractPeriod.java
@@ -5,6 +5,10 @@ import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 /**
  * @Project: mall-cloud
@@ -21,6 +25,21 @@ public abstract class AbstractPeriod implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
+     * 默认时间格式
+     */
+    public static final String PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * java.util.Date 时间格式化器
+     */
+    public static final ThreadLocal<SimpleDateFormat> DATE_FORMATTER = ThreadLocal.withInitial(() -> new SimpleDateFormat(PATTERN));
+
+    /**
+     * java.util.LocalDateTime 时间格式化器
+     */
+    public static final DateTimeFormatter LOCAL_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(PATTERN);
+
+    /**
      * 开始时间
      */
     private Object start;
@@ -29,4 +48,40 @@ public abstract class AbstractPeriod implements Serializable {
      * 结束时间
      */
     private Object end;
+
+    /**
+     * 判断两个时间区间是否相等
+     *
+     * @param other
+     * @return
+     */
+    public boolean contentEquals(final AbstractPeriod other) {
+        // 获取 this 区间的开始时间和结束时间
+        String[] thisTimes = times(this);
+        // 获取 other 区间的开始时间和结束时间
+        String[] otherTimes = times(other);
+        // 分别判断开始时间和结束时间
+        return Objects.equals(thisTimes[0], otherTimes[0]) && Objects.equals(thisTimes[1], otherTimes[1]);
+    }
+
+    /**
+     * 获取时间区间的开始时间和结束时间
+     *
+     * @param period
+     * @return
+     */
+    private String[] times(AbstractPeriod period) {
+        // times[0] 开始时间, times[1] 结束时间
+        String[] times = new String[2];
+        if (period instanceof DatePeriod) {
+            times[0] = DATE_FORMATTER.get().format(((DatePeriod) period).getStart());
+            times[1] = DATE_FORMATTER.get().format(((DatePeriod) period).getEnd());
+        } else if (period instanceof LocalDateTimePeriod) {
+            times[0] = ((LocalDateTime) period.getStart()).format(LOCAL_DATE_TIME_FORMATTER);
+            times[1] = ((LocalDateTime) period.getEnd()).format(LOCAL_DATE_TIME_FORMATTER);
+        } else {
+            throw new RuntimeException("Could not format the period!");
+        }
+        return times;
+    }
 }
