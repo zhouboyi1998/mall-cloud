de5e15065c1cde9a007fa82b90491ab340626c0d
zhouboyi <1144188685@qq.com>
2023-10-19 17:49:32 +0800

    enhance(qiniu): 将七牛云授权管理器、存储配置、存储桶管理器、文件上传管理器注入到Spring容器中

--- mall-middleware/mall-qiniu/src/main/java/com/cafe/qiniu/config/QiniuConfig.java
+++ mall-middleware/mall-qiniu/src/main/java/com/cafe/qiniu/config/QiniuConfig.java
@@ -1,31 +1,68 @@
 package com.cafe.qiniu.config;
 
-import com.qiniu.storage.Region;
+import com.cafe.qiniu.enumeration.RegionEnum;
+import com.cafe.qiniu.property.QiniuProperties;
+import com.qiniu.storage.BucketManager;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.util.Auth;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @Project: mall-cloud
  * @Package: com.cafe.qiniu.config
  * @Author: zhouboyi
  * @Date: 2023/10/18 17:41
- * @Description:
+ * @Description: 七牛云配置类
  */
 @Configuration
 public class QiniuConfig {
 
+    private final QiniuProperties qiniuProperties;
+
+    @Autowired
+    public QiniuConfig(QiniuProperties qiniuProperties) {
+        this.qiniuProperties = qiniuProperties;
+    }
+
+    /**
+     * 七牛云授权管理器
+     *
+     * @return
+     */
     @Bean
-    public Map<String, Region> regionMap() {
-        Map<String, Region> regionMap = new HashMap<>(8);
-        regionMap.put("huanan", Region.huanan());
-        regionMap.put("huadong", Region.huadong());
-        regionMap.put("huabei", Region.huabei());
-        regionMap.put("beimei", Region.beimei());
-        regionMap.put("xinjiapo", Region.xinjiapo());
-        regionMap.put("huadong2", Region.huadongZheJiang2());
-        return regionMap;
+    public Auth auth() {
+        return Auth.create(qiniuProperties.getAccessKey(), qiniuProperties.getSecretKey());
+    }
+
+    /**
+     * 存储配置
+     *
+     * @return
+     */
+    @Bean
+    public com.qiniu.storage.Configuration configuration() {
+        return new com.qiniu.storage.Configuration(RegionEnum.getRegionByName(qiniuProperties.getRegion()));
+    }
+
+    /**
+     * 存储桶管理器
+     *
+     * @return
+     */
+    @Bean
+    public BucketManager bucketManager() {
+        return new BucketManager(auth(), configuration());
+    }
+
+    /**
+     * 文件上传管理器
+     *
+     * @return
+     */
+    @Bean
+    public UploadManager uploadManager() {
+        return new UploadManager(configuration());
     }
 }
