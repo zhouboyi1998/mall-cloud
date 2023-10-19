cbf890911038c03353e878ea9bbb275da4184e0c
zhouboyi <1144188685@qq.com>
2023-10-19 15:58:37 +0800

    improve(aop): AOP工具类获取请求参数方法，跳过HttpServlet请求、响应类型参数

--- mall-common/mall-common-util/src/main/java/com/cafe/common/util/aop/AOPUtil.java
+++ mall-common/mall-common-util/src/main/java/com/cafe/common/util/aop/AOPUtil.java
@@ -7,6 +7,8 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,17 +41,24 @@ public class AOPUtil {
 
         // 遍历组装成 JSON 格式
         for (int i = 0; i < valueList.size(); i++) {
-            args.append(StringConstant.DOUBLE_QUOTATION).append(keyList.get(i))
-                .append(StringConstant.DOUBLE_QUOTATION).append(StringConstant.COLON);
+            // 获取参数值
             Object value = valueList.get(i);
+
+            if (value instanceof HttpServletRequest || value instanceof HttpServletResponse) {
+                // 跳过 HttpServlet 请求/响应类型参数
+                continue;
+            }
+
+            // 拼接参数名
+            args.append(StringConstant.DOUBLE_QUOTATION).append(keyList.get(i)).append(StringConstant.DOUBLE_QUOTATION).append(StringConstant.COLON);
+
+            // 拼接参数值
             if (value instanceof Number || value instanceof Character || value instanceof Boolean || value instanceof CharSequence) {
                 // 基本类型、字符串类型的参数直接拼接
-                args.append(StringConstant.DOUBLE_QUOTATION).append(value)
-                    .append(StringConstant.DOUBLE_QUOTATION).append(StringConstant.COMMA);
+                args.append(StringConstant.DOUBLE_QUOTATION).append(value).append(StringConstant.DOUBLE_QUOTATION).append(StringConstant.COMMA);
             } else if (value instanceof MultipartFile) {
                 // 文件类型的参数, 拼接文件名称
-                args.append(StringConstant.DOUBLE_QUOTATION).append(((MultipartFile) value).getOriginalFilename())
-                    .append(StringConstant.DOUBLE_QUOTATION).append(StringConstant.COMMA);
+                args.append(StringConstant.DOUBLE_QUOTATION).append(((MultipartFile) value).getOriginalFilename()).append(StringConstant.DOUBLE_QUOTATION).append(StringConstant.COMMA);
             } else {
                 // 引用类型的参数转换成 JSON 字符串再拼接
                 args.append(JacksonUtil.writeValueAsString(value)).append(StringConstant.COMMA);
