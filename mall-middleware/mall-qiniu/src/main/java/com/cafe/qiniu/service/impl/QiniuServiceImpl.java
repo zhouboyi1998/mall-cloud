6811a76e4a73b41830874b2e518f7e9ab9113c7e
zhouboyi <1144188685@qq.com>
2023-10-19 17:49:32 +0800

    enhance(qiniu): 将七牛云授权管理器、存储配置、存储桶管理器、文件上传管理器注入到Spring容器中

--- mall-middleware/mall-qiniu/src/main/java/com/cafe/qiniu/service/impl/QiniuServiceImpl.java
+++ mall-middleware/mall-qiniu/src/main/java/com/cafe/qiniu/service/impl/QiniuServiceImpl.java
@@ -2,12 +2,9 @@ package com.cafe.qiniu.service.impl;
 
 import com.cafe.common.constant.pool.StringConstant;
 import com.cafe.id.feign.IDFeign;
-import com.cafe.qiniu.property.QiniuProperties;
 import com.cafe.qiniu.service.QiniuService;
 import com.qiniu.http.Response;
 import com.qiniu.storage.BucketManager;
-import com.qiniu.storage.Configuration;
-import com.qiniu.storage.Region;
 import com.qiniu.storage.UploadManager;
 import com.qiniu.util.Auth;
 import org.slf4j.Logger;
@@ -16,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -31,25 +27,29 @@ public class QiniuServiceImpl implements QiniuService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(QiniuServiceImpl.class);
 
-    private final QiniuProperties qiniuProperties;
+    private final Auth auth;
+
+    private final BucketManager bucketManager;
+
+    private final UploadManager uploadManager;
 
     private final IDFeign idFeign;
 
-    private final Map<String, Region> regionMap;
-
     @Autowired
     public QiniuServiceImpl(
-        QiniuProperties qiniuProperties,
-        IDFeign idFeign,
-        Map<String, Region> regionMap
+        Auth auth,
+        BucketManager bucketManager,
+        UploadManager uploadManager,
+        IDFeign idFeign
     ) {
-        this.qiniuProperties = qiniuProperties;
+        this.auth = auth;
+        this.bucketManager = bucketManager;
+        this.uploadManager = uploadManager;
         this.idFeign = idFeign;
-        this.regionMap = regionMap;
     }
 
     @Override
-    public String upload(String region, String bucket, MultipartFile file) throws Exception {
+    public String upload(String bucket, MultipartFile file) throws Exception {
         // 使用雪花算法生成文件名
         StringBuilder filename = new StringBuilder(Objects.requireNonNull(idFeign.nextId().getBody()).toString());
         // 获取文件原名
@@ -59,30 +59,19 @@ public class QiniuServiceImpl implements QiniuService {
             filename.append(originalFilename.substring(originalFilename.indexOf(StringConstant.POINT)));
         }
 
-        // 生成令牌
-        Auth auth = Auth.create(qiniuProperties.getAccessKey(), qiniuProperties.getSecretKey());
+        // 生成文件上传令牌
         String uploadToken = auth.uploadToken(bucket);
-
         // 上传文件到七牛云
-        Configuration configuration = new Configuration(regionMap.get(region));
-        UploadManager uploadManager = new UploadManager(configuration);
         Response response = uploadManager.put(file.getBytes(), filename.toString(), uploadToken);
-
         LOGGER.info("QiniuServiceImpl.upload(): response -> {}", response);
 
         return StringConstant.SLASH + bucket + StringConstant.SLASH + filename;
     }
 
     @Override
-    public void delete(String region, String bucket, String filename) throws Exception {
-        // 生成令牌
-        Auth auth = Auth.create(qiniuProperties.getAccessKey(), qiniuProperties.getSecretKey());
-
+    public void delete(String bucket, String filename) throws Exception {
         // 删除文件
-        Configuration configuration = new Configuration(regionMap.get(region));
-        BucketManager bucketManager = new BucketManager(auth, configuration);
         Response response = bucketManager.delete(bucket, filename);
-
         LOGGER.info("QiniuServiceImpl.delete(): response -> {}", response);
     }
 }
