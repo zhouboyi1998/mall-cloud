9ac78155560434f6aa88c6c773c09bf40d87aa90
zhouboyi <1144188685@qq.com>
2023-10-23 15:32:04 +0800

    improve(area): 区域模型添加行政区划编码字段

--- mall-business/mall-member/src/main/java/com/cafe/member/service/AddressService.java
+++ mall-business/mall-member/src/main/java/com/cafe/member/service/AddressService.java
@@ -1,15 +1,15 @@
 package com.cafe.member.service;
 
-import com.cafe.member.model.MemberAddress;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.cafe.member.model.Address;
 
 /**
  * @Project: mall-cloud
  * @Package: com.cafe.member.service
  * @Author: zhouboyi
  * @Date: 2022-12-29
- * @Description: 会员收货地址业务接口
+ * @Description: 收货地址业务接口
  */
-public interface MemberAddressService extends IService<MemberAddress> {
+public interface AddressService extends IService<Address> {
 
 }
