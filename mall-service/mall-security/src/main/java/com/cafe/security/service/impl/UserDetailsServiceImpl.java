18a6dac6df9be0dcb6fcb18152f983728286979e
zhouboyi <1144188685@qq.com>
2023-12-28 16:19:41 +0800

    perf(security): 使用Optional工具类优化用户详细信息加载相关代码

--- mall-service/mall-security/src/main/java/com/cafe/security/service/impl/UserDetailsServiceImpl.java
+++ mall-service/mall-security/src/main/java/com/cafe/security/service/impl/UserDetailsServiceImpl.java
@@ -11,6 +11,7 @@ import com.cafe.user.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AccountExpiredException;
 import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.authentication.DisabledException;
@@ -21,15 +22,14 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @Project: mall-cloud
  * @Package: com.cafe.security.service.impl
  * @Author: zhouboyi
  * @Date: 2022/5/6 11:19
- * @Description: 用户详细信息组装服务
+ * @Description: 用户详细信息加载实现类
  */
 @Service
 public class UserDetailsServiceImpl implements UserDetailsExtensionService {
@@ -51,54 +51,50 @@ public class UserDetailsServiceImpl implements UserDetailsExtensionService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // 从 Request Parameter 中获取客户端id
+        // 从请求参数中获取客户端id
         String clientId = request.getParameter(RequestConstant.Parameter.CLIENT_ID);
 
-        // 根据用户名和客户端id查询用户详情
-        User user = userFeign.detailByUsername(username, clientId).getBody();
+        // 根据用户名和客户端id查询用户
+        User user = Optional.ofNullable(userFeign.detailByUsername(username, clientId))
+            .map(ResponseEntity::getBody)
+            .orElseThrow(() -> new UsernameNotFoundException(HttpStatusEnum.USERNAME_NOT_FOUND.getReasonPhrase()));
 
-        if (Objects.nonNull(user)) {
-            // 根据用户id查询角色名称列表
-            List<String> roleNameList = roleFeign.listRoleName(user.getId()).getBody();
-            if (Objects.isNull(roleNameList)) {
-                throw new UsernameNotFoundException(HttpStatusEnum.ROLE_NOT_FOUND.getReasonPhrase());
-            }
-            // 角色名称列表转换为数组形式
-            String[] roleNameArray = roleNameList.toArray(new String[0]);
-            UserInfo userDetails = new UserInfo(user.getId(), user.getUsername(), user.getPassword(), AuthorityUtils.createAuthorityList(roleNameArray));
-            // 校验用户状态
-            validateUserDetails(userDetails);
-            LOGGER.info("UserDetailsServiceImpl.loadUserByUsername(): username -> {}, client id -> {}", username, clientId);
-            return userDetails;
-        } else {
-            throw new UsernameNotFoundException(HttpStatusEnum.USERNAME_NOT_FOUND.getReasonPhrase());
-        }
+        // 根据用户id查询角色名称列表
+        String[] roleNameArray = Optional.ofNullable(roleFeign.listRoleName(user.getId()))
+            .map(ResponseEntity::getBody)
+            .map(roleNameList -> roleNameList.toArray(new String[0]))
+            .orElseThrow(() -> new UsernameNotFoundException(HttpStatusEnum.ROLE_UNASSIGNED.getReasonPhrase()));
+
+        // 新建用户详细信息
+        UserInfo userDetails = new UserInfo(user.getId(), user.getUsername(), user.getPassword(), AuthorityUtils.createAuthorityList(roleNameArray));
+        // 校验用户状态
+        validateUserDetails(userDetails);
+        LOGGER.info("UserDetailsServiceImpl.loadUserByUsername(): username -> {}, client id -> {}", username, clientId);
+        return userDetails;
     }
 
     @Override
     public UserDetails loadUserByMobile(String mobile) throws MobileNotFoundException {
-        // 从 Request Parameter 中获取客户端id
+        // 从请求参数中获取客户端id
         String clientId = request.getParameter(RequestConstant.Parameter.CLIENT_ID);
 
-        // 根据手机号和客户端id查询用户详情
-        User user = userFeign.detailByMobile(mobile, clientId).getBody();
+        // 根据手机号和客户端id查询用户
+        User user = Optional.ofNullable(userFeign.detailByMobile(mobile, clientId))
+            .map(ResponseEntity::getBody)
+            .orElseThrow(() -> new MobileNotFoundException(HttpStatusEnum.MOBILE_NOT_FOUND.getReasonPhrase()));
 
-        if (Objects.nonNull(user)) {
-            // 根据用户id查询角色名称列表
-            List<String> roleNameList = roleFeign.listRoleName(user.getId()).getBody();
-            if (Objects.isNull(roleNameList)) {
-                throw new UsernameNotFoundException(HttpStatusEnum.ROLE_NOT_FOUND.getReasonPhrase());
-            }
-            // 角色名称列表转换为数组形式
-            String[] roleNameArray = roleNameList.toArray(new String[0]);
-            UserInfo userDetails = new UserInfo(user.getId(), user.getUsername(), user.getMobile(), user.getPassword(), AuthorityUtils.createAuthorityList(roleNameArray));
-            // 校验用户状态
-            validateUserDetails(userDetails);
-            LOGGER.info("UserDetailsServiceImpl.loadUserByMobile(): mobile -> {}, client id -> {}", mobile, clientId);
-            return userDetails;
-        } else {
-            throw new UsernameNotFoundException(HttpStatusEnum.USERNAME_NOT_FOUND.getReasonPhrase());
-        }
+        // 根据用户id查询角色名称列表
+        String[] roleNameArray = Optional.ofNullable(roleFeign.listRoleName(user.getId()))
+            .map(ResponseEntity::getBody)
+            .map(roleNameList -> roleNameList.toArray(new String[0]))
+            .orElseThrow(() -> new MobileNotFoundException(HttpStatusEnum.ROLE_UNASSIGNED.getReasonPhrase()));
+
+        // 新建用户详细信息
+        UserInfo userDetails = new UserInfo(user.getId(), user.getUsername(), user.getMobile(), user.getPassword(), AuthorityUtils.createAuthorityList(roleNameArray));
+        // 校验用户状态
+        validateUserDetails(userDetails);
+        LOGGER.info("UserDetailsServiceImpl.loadUserByMobile(): mobile -> {}, client id -> {}", mobile, clientId);
+        return userDetails;
     }
 
     /**
