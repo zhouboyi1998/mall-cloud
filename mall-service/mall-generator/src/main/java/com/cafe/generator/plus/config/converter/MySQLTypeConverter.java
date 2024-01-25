5804bc2adb2ac5f5e6662904c991f63dd73a22b2
zhouboyi <1144188685@qq.com>
2024-01-25 16:19:17 +0800

    improve(generator): 优化MyBatis-Plus代码生成器数据源配置相关代码

---
+++ mall-service/mall-generator/src/main/java/com/cafe/generator/plus/config/converter/MySQLTypeConverter.java
@@ -0,0 +1,35 @@
+package com.cafe.generator.plus.config.converter;
+
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.converts.MySqlTypeConvert;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
+import com.baomidou.mybatisplus.generator.config.rules.IColumnType;
+import com.cafe.common.enumeration.database.ColumnTypeEnum;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.generator.plus.config.converter
+ * @Author: zhouboyi
+ * @Date: 2024/1/25 15:37
+ * @Description: MySQL 数据库字段类型转换器
+ */
+public class MySQLTypeConverter extends MySqlTypeConvert {
+
+    @Override
+    public IColumnType processTypeConvert(GlobalConfig config, String fieldType) {
+        switch (ColumnTypeEnum.getColumnType(fieldType.toUpperCase())) {
+            case DECIMAL:
+            case NUMERIC:
+                return DbColumnType.BIG_DECIMAL;
+            case TIMESTAMP:
+            case DATETIME:
+                return DbColumnType.LOCAL_DATE_TIME;
+            case DATE:
+                return DbColumnType.LOCAL_DATE;
+            case TIME:
+                return DbColumnType.LOCAL_TIME;
+            default:
+                return super.processTypeConvert(config, fieldType);
+        }
+    }
+}
