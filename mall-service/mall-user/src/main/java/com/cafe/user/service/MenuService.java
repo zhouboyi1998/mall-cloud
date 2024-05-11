5cc4be21c9b4d2c8cfd8234e3245c3697a65248c
zhouboyi <1144188685@qq.com>
2024-05-11 10:38:59 +0800

    feat(tree): 添加树形结构和树形工具类

--- mall-service/mall-user/src/main/java/com/cafe/user/service/MenuService.java
+++ mall-service/mall-user/src/main/java/com/cafe/user/service/MenuService.java
@@ -1,8 +1,8 @@
 package com.cafe.user.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.cafe.common.lang.tree.Tree;
 import com.cafe.user.model.Menu;
-import com.cafe.user.vo.MenuTreeVO;
 
 import java.util.List;
 
@@ -16,10 +16,10 @@ import java.util.List;
 public interface MenuService extends IService<Menu> {
 
     /**
-     * 根据权限列表获取菜单树
+     * 根据权限列表查询菜单树
      *
      * @param authorities 权限列表
      * @return
      */
-    List<MenuTreeVO> tree(List<String> authorities);
+    List<Tree> treeList(List<String> authorities);
 }
