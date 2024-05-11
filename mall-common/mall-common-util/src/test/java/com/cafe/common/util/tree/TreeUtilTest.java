366d7bbb168e61ab962886d1613ff57c5dac5c9a
zhouboyi <1144188685@qq.com>
2024-05-11 10:38:59 +0800

    feat(tree): 添加树形结构和树形工具类

---
+++ mall-common/mall-common-util/src/test/java/com/cafe/common/util/tree/TreeUtilTest.java
@@ -0,0 +1,57 @@
+package com.cafe.common.util.tree;
+
+import com.cafe.common.lang.tree.Tree;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.common.util.tree
+ * @Author: zhouboyi
+ * @Date: 2024/5/11 9:49
+ * @Description:
+ */
+class TreeUtilTest {
+
+    private static final List<Tree> NODE_LIST = new ArrayList<>();
+
+    static {
+        NODE_LIST.add(new Tree().setId(1L).setParentId(0L));
+        NODE_LIST.add(new Tree().setId(2L).setParentId(0L));
+        NODE_LIST.add(new Tree().setId(3L).setParentId(0L));
+
+        NODE_LIST.add(new Tree().setId(11L).setParentId(1L));
+        NODE_LIST.add(new Tree().setId(12L).setParentId(1L));
+        NODE_LIST.add(new Tree().setId(13L).setParentId(1L));
+
+        NODE_LIST.add(new Tree().setId(21L).setParentId(2L));
+        NODE_LIST.add(new Tree().setId(22L).setParentId(2L));
+
+        NODE_LIST.add(new Tree().setId(111L).setParentId(11L));
+        NODE_LIST.add(new Tree().setId(112L).setParentId(11L));
+        NODE_LIST.add(new Tree().setId(113L).setParentId(11L));
+
+        NODE_LIST.add(new Tree().setId(211L).setParentId(21L));
+        NODE_LIST.add(new Tree().setId(212L).setParentId(21L));
+    }
+
+    @Test
+    void buildTreeList() {
+        List<Tree> treeList = TreeUtil.buildTreeList(NODE_LIST);
+        System.out.println(treeList);
+    }
+
+    @Test
+    void buildTreeList2() {
+        List<Tree> treeList = TreeUtil.buildTreeList(NODE_LIST, 1L);
+        System.out.println(treeList);
+    }
+
+    @Test
+    void buildTree() {
+        Tree tree = TreeUtil.buildTree(NODE_LIST, 2L);
+        System.out.println(tree);
+    }
+}
--- mall-service/mall-user/src/main/java/com/cafe/user/service/impl/MenuServiceImpl.java
+++ mall-service/mall-user/src/main/java/com/cafe/user/service/impl/MenuServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cafe.user.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cafe.common.lang.tree.Tree;
+import com.cafe.common.util.tree.TreeUtil;
 import com.cafe.user.mapper.MenuMapper;
 import com.cafe.user.model.Menu;
 import com.cafe.user.service.MenuService;
@@ -9,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @Project: mall-cloud
@@ -29,34 +30,10 @@ public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements Me
     }
 
     @Override
-    public List<MenuTreeVO> tree(List<String> authorities) {
-        // 根据权限列表获取对应的菜单树VO
-        List<MenuTreeVO> menuList = menuMapper.tree(authorities);
-
+    public List<Tree> treeList(List<String> authorities) {
+        // 根据权限列表查询菜单列表
+        List<MenuTreeVO> menuList = menuMapper.selectTreeVOList(authorities);
         // 组装成树形格式
-        return menuList.stream()
-            // 筛选出所有一级菜单
-            .filter(menuTreeVO -> menuTreeVO.getParentId().equals(0L))
-            // 为所有一级菜单组装子菜单树
-            .map(menuTreeVO -> menuTreeVO.setChildren(buildSubtree(menuTreeVO, menuList)))
-            // 重新组装成 List
-            .collect(Collectors.toList());
-    }
-
-    /**
-     * 为当前菜单组装子菜单树
-     *
-     * @param node     当前菜单
-     * @param menuList 所有菜单
-     * @return
-     */
-    private List<MenuTreeVO> buildSubtree(MenuTreeVO node, List<MenuTreeVO> menuList) {
-        return menuList.stream()
-            // 从所有菜单中筛选出当前菜单的子菜单
-            .filter(menuTreeVO -> menuTreeVO.getParentId().equals(node.getId()))
-            // 递归调用组装树形结构
-            .map(menuTreeVO -> menuTreeVO.setChildren(buildSubtree(menuTreeVO, menuList)))
-            // 重新组装成 List
-            .collect(Collectors.toList());
+        return TreeUtil.buildTreeList(menuList);
     }
 }
