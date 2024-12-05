74307f0c65dad21caec2923f028a1f5d21b3b09c
zhouboyi <1144188685@qq.com>
2024-12-05 23:17:43 +0800

    style(jwk): 修改JWK相关接口的请求路径

--- mall-service/mall-security/src/main/java/com/cafe/security/service/impl/JWKServiceImpl.java
+++ mall-service/mall-security/src/main/java/com/cafe/security/service/impl/JWKServiceImpl.java
@@ -1,6 +1,6 @@
 package com.cafe.security.service.impl;
 
-import com.cafe.security.service.JWKSetService;
+import com.cafe.security.service.JWKService;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.security.KeyPair;
 import java.security.interfaces.RSAPublicKey;
+import java.util.Base64;
 import java.util.Map;
 
 /**
@@ -15,11 +16,11 @@ import java.util.Map;
  * @Package: com.cafe.security.service.impl
  * @Author: zhouboyi
  * @Date: 2022/7/8 10:09
- * @Description:
+ * @Description: JWK 业务实现类
  */
 @RequiredArgsConstructor
 @Service
-public class JWKSetServiceImpl implements JWKSetService {
+public class JWKServiceImpl implements JWKService {
 
     /**
      * 密钥对
@@ -27,12 +28,18 @@ public class JWKSetServiceImpl implements JWKSetService {
     private final KeyPair keyPair;
 
     @Override
-    public Map<String, Object> rsa() {
-        // 从证书中获取 RSA 公钥
+    public Map<String, Object> jwkSet() {
+        // 从密钥对中获取公钥, 转换成 RSA 公钥
         RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
-        // 使用 RSA 公钥生成 RSA JWK
+        // 使用 RSA 公钥生成 JWK
         RSAKey key = new RSAKey.Builder(publicKey).build();
-        // 生成 JWKSet 并返回
+        // 封装成 JWKSet 格式返回
         return new JWKSet(key).toJSONObject();
+    }
+
+    @Override
+    public String base64() {
+        // 从密钥对中获取公钥, 封装成 Base64 编码返回
+        return Base64.getEncoder().encodeToString(keyPair.getPublic().getEncoded());
     }
 }
