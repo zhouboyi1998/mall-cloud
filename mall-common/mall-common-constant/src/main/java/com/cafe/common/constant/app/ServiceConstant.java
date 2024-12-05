20f3ee67304f6514b9ffa92f72a59dda588485db
zhouboyi <1144188685@qq.com>
2024-12-05 23:21:33 +0800

    refactor(response): 修改全局返回结果转换器判断JWK接口的逻辑

---
+++ mall-common/mall-common-constant/src/main/java/com/cafe/common/constant/app/ServiceConstant.java
@@ -0,0 +1,16 @@
+package com.cafe.common.constant.app;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.common.constant.app
+ * @Author: zhouboyi
+ * @Date: 2024/12/5 18:37
+ * @Description: 服务相关常量
+ */
+public class ServiceConstant {
+
+    /**
+     * 安全服务
+     */
+    public static final String MALL_SECURITY = "mall-security";
+}
