a944852af73574a055d1da82b0ccf2150bf24098
zhouboyi <zby02126265@alibaba-inc.com>
2025-06-13 09:57:32 +0800

    feat(meilisearch): 添加搜索商品索引接口

--- mall-common/mall-common-constant/src/main/java/com/cafe/common/constant/meilisearch/MeiliSearchConstant.java
+++ mall-common/mall-common-constant/src/main/java/com/cafe/common/constant/meilisearch/MeiliSearchConstant.java
@@ -1,5 +1,7 @@
 package com.cafe.common.constant.meilisearch;
 
+import com.cafe.common.constant.app.FieldConstant;
+
 /**
  * @Project: mall-cloud
  * @Package: com.cafe.common.constant.meilisearch
@@ -22,6 +24,48 @@ public class MeiliSearchConstant {
         /**
          * 主键字段
          */
-        public static final String PRIMARY_KEY = "id";
+        public static final String PRIMARY_KEY = FieldConstant.ID;
+
+        /**
+         * 全文搜索字段
+         */
+        public static final String[] SEARCH_FIELD = {
+            FieldConstant.SKU_NAME,
+            FieldConstant.SPU_NAME,
+            FieldConstant.BRAND_NAME,
+            FieldConstant.CATEGORY_NAME,
+            FieldConstant.SPECIFICATION,
+            FieldConstant.SHOP_NAME
+        };
+
+        /**
+         * 过滤字段
+         */
+        public static final String[] FILTER_FIELD = {
+            FieldConstant.ID,
+            FieldConstant.SPU_ID,
+            FieldConstant.BRAND_ID,
+            FieldConstant.CATEGORY_ID,
+            FieldConstant.SHOP_ID
+        };
+
+        /**
+         * 默认排序字段
+         */
+        public static final String DEFAULT_SORT_FIELD = FieldConstant.SALE;
+
+        /**
+         * 可排序字段
+         */
+        public static final String[] SORT_FIELD = {
+            FieldConstant.ORIGINAL_PRICE,
+            FieldConstant.DISCOUNT_PRICE,
+            FieldConstant.SECKILL_PRICE,
+            FieldConstant.SALE,
+            FieldConstant.TOTAL_REVIEW,
+            FieldConstant.GOOD_REVIEW,
+            FieldConstant.MEDIUM_REVIEW,
+            FieldConstant.BAD_REVIEW
+        };
     }
 }
