a2d935ba813db94de6498f8122493446e176ba6d
zhouboyi <1144188685@qq.com>
2025-06-16 22:42:10 +0800

    feat(opensearch): OpenSearch模块新增商品索引相关接口

---
+++ mall-common/mall-common-constant/src/main/java/com/cafe/common/constant/opensearch/OpenSearchConstant.java
@@ -0,0 +1,22 @@
+package com.cafe.common.constant.opensearch;
+
+/**
+ * @Project: mall-cloud
+ * @Package: com.cafe.common.constant.opensearch
+ * @Author: zhouboyi
+ * @Date: 2025/6/16 21:20
+ * @Description: OpenSearch 相关常量
+ */
+public class OpenSearchConstant {
+
+    /**
+     * 商品索引
+     */
+    public static class Goods {
+
+        /**
+         * 索引名称
+         */
+        public static final String INDEX = "goods-index";
+    }
+}
