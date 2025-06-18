751b82881c4ae0c10df09f1c47a8d92437227092
zhouboyi <1144188685@qq.com>
2025-06-18 22:10:14 +0800

    config(search): 添加ElasticSearch、OpenSearch自动装配配置类

--- mall-infrastructure/mall-infrastructure-elasticsearch/src/main/java/com/cafe/infrastructure/elasticsearch/config/OpenSearchClientAutoConfiguration.java
+++ mall-infrastructure/mall-infrastructure-elasticsearch/src/main/java/com/cafe/infrastructure/elasticsearch/config/OpenSearchClientAutoConfiguration.java
@@ -1,6 +1,6 @@
-package com.cafe.opensearch.config;
+package com.cafe.infrastructure.elasticsearch.config;
 
-import com.cafe.opensearch.property.OpenSearchProperties;
+import com.cafe.infrastructure.elasticsearch.property.OpenSearchProperties;
 import lombok.RequiredArgsConstructor;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
@@ -9,19 +9,23 @@ import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @Project: mall-cloud
- * @Package: com.cafe.opensearch.config
+ * @Package: com.cafe.infrastructure.elasticsearch.config
  * @Author: zhouboyi
  * @Date: 2025/6/15 6:15
- * @Description: OpenSearch 配置类
+ * @Description: OpenSearch Client 自动装配配置类
  */
 @RequiredArgsConstructor
 @Configuration
-public class OpenSearchConfig {
+@EnableConfigurationProperties(value = OpenSearchProperties.class)
+@ConditionalOnClass(value = RestHighLevelClient.class)
+public class OpenSearchClientAutoConfiguration {
 
     private final OpenSearchProperties openSearchProperties;
 
@@ -29,7 +33,7 @@ public class OpenSearchConfig {
     public RestHighLevelClient restHighLevelClient() {
         CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(openSearchProperties.getUsername(), openSearchProperties.getPassword()));
-        return new RestHighLevelClient(RestClient.builder(new HttpHost(openSearchProperties.getHost(), openSearchProperties.getPort(), openSearchProperties.getProtocol()))
+        return new RestHighLevelClient(RestClient.builder(new HttpHost(openSearchProperties.getHost(), openSearchProperties.getPort(), openSearchProperties.getScheme()))
             .setHttpClientConfigCallback(httpClientBuilder -> httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)));
     }
 }
