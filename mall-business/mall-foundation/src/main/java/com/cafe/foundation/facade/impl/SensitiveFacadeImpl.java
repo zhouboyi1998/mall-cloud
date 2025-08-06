ce76eefeadf5fcfd7f8f2788bd10b798c74f4487
zhouboyi <zby02126265@alibaba-inc.com>
2025-08-06 18:46:16 +0800

    enhance(sensitive): 敏感词字典树使用读写锁确保线程安全

--- mall-business/mall-foundation/src/main/java/com/cafe/foundation/facade/impl/SensitiveFacadeImpl.java
+++ mall-business/mall-foundation/src/main/java/com/cafe/foundation/facade/impl/SensitiveFacadeImpl.java
@@ -15,6 +15,8 @@ import org.springframework.util.CollectionUtils;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * @Project: mall-cloud
@@ -28,6 +30,11 @@ import java.util.List;
 public class SensitiveFacadeImpl implements SensitiveFacade {
 
     /**
+     * 读写锁
+     */
+    private static final ReadWriteLock READ_WRITE_LOCK = new ReentrantReadWriteLock();
+
+    /**
      * 敏感词字典树根节点
      */
     private static TrieNode<Character> SENSITIVE_TRIE_ROOT;
@@ -39,46 +46,67 @@ public class SensitiveFacadeImpl implements SensitiveFacade {
     @PostConstruct
     @Override
     public void initSensitiveTrie() {
-        // 查询所有启用的敏感词
-        String[] sensitiveWordArray = sensitiveService.lambdaQuery()
-            .eq(Sensitive::getStatus, SensitiveConstant.Status.ENABLE)
-            .list()
-            .stream()
-            .map(Sensitive::getSensitiveWord)
-            // 忽略大小写
-            .map(String::toLowerCase)
-            .toArray(String[]::new);
-        // 构建敏感词字典树
-        SENSITIVE_TRIE_ROOT = AhoCorasick.buildTrie(sensitiveWordArray);
+        // 获取写锁
+        READ_WRITE_LOCK.writeLock().lock();
+        try {
+            // 查询所有启用的敏感词
+            String[] sensitiveWordArray = sensitiveService.lambdaQuery()
+                .eq(Sensitive::getStatus, SensitiveConstant.Status.ENABLE)
+                .list()
+                .stream()
+                .map(Sensitive::getSensitiveWord)
+                // 忽略大小写
+                .map(String::toLowerCase)
+                .toArray(String[]::new);
+            // 构建敏感词字典树
+            SENSITIVE_TRIE_ROOT = AhoCorasick.buildTrie(sensitiveWordArray);
+        } finally {
+            // 释放写锁
+            READ_WRITE_LOCK.writeLock().unlock();
+        }
     }
 
     @Override
     public void addSensitiveWords(List<Long> sensitiveIds) {
-        // 查询敏感词
-        String[] sensitiveWordArray = sensitiveService.lambdaQuery()
-            .in(Sensitive::getId, sensitiveIds)
-            .eq(Sensitive::getStatus, SensitiveConstant.Status.ENABLE)
-            .list()
-            .stream()
-            .map(Sensitive::getSensitiveWord)
-            // 忽略大小写
-            .map(String::toLowerCase)
-            .toArray(String[]::new);
-        // 添加敏感词到字典树中
-        AhoCorasick.insertKeywords(SENSITIVE_TRIE_ROOT, sensitiveWordArray);
+        // 获取写锁
+        READ_WRITE_LOCK.writeLock().lock();
+        try {
+            // 查询敏感词
+            String[] sensitiveWordArray = sensitiveService.lambdaQuery()
+                .in(Sensitive::getId, sensitiveIds)
+                .eq(Sensitive::getStatus, SensitiveConstant.Status.ENABLE)
+                .list()
+                .stream()
+                .map(Sensitive::getSensitiveWord)
+                // 忽略大小写
+                .map(String::toLowerCase)
+                .toArray(String[]::new);
+            // 添加敏感词到字典树中
+            AhoCorasick.insertKeywords(SENSITIVE_TRIE_ROOT, sensitiveWordArray);
+        } finally {
+            // 释放写锁
+            READ_WRITE_LOCK.writeLock().unlock();
+        }
     }
 
     @Override
     public List<String> matchText(String text) {
-        // 忽略大小写
-        text = text.toLowerCase();
-        // 移除干扰符
-        List<Interference> interferenceList = interferenceService.enableList();
-        for (Interference interference : interferenceList) {
-            text = text.replaceAll(interference.getInterferenceSymbol(), StringConstant.EMPTY);
+        // 获取读锁
+        READ_WRITE_LOCK.readLock().lock();
+        try {
+            // 忽略大小写
+            text = text.toLowerCase();
+            // 移除干扰符
+            List<Interference> interferenceList = interferenceService.enableList();
+            for (Interference interference : interferenceList) {
+                text = text.replaceAll(interference.getInterferenceSymbol(), StringConstant.EMPTY);
+            }
+            // 匹配文本中的所有敏感词
+            return AhoCorasick.matchText(SENSITIVE_TRIE_ROOT, text);
+        } finally {
+            // 释放读锁
+            READ_WRITE_LOCK.readLock().unlock();
         }
-        // 匹配文本中的所有敏感词
-        return AhoCorasick.matchText(SENSITIVE_TRIE_ROOT, text);
     }
 
     @Override
