1df2fc165371747b6ab3302da684a474aa849750
zhouboyi <zby02126265@alibaba-inc.com>
2025-09-24 10:00:41 +0800

    config(id): 添加全局工作机器ID、数据中心ID配置项

--- mall-service/mall-id/src/main/java/com/cafe/id/property/IDProperties.java
+++ mall-service/mall-id/src/main/java/com/cafe/id/property/IDProperties.java
@@ -6,6 +6,11 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 /**
  * @Project: mall-cloud
  * @Package: com.cafe.id.property
@@ -24,6 +29,16 @@ public class IDProperties {
      */
     private IDGenerator defaultGenerator = IDGenerator.SNOWFLAKE;
 
+    /**
+     * 工作机器ID (全局配置)
+     */
+    private Long workerId;
+
+    /**
+     * 数据中心ID (全局配置)
+     */
+    private Long datacenterId;
+
     private Snowflake snowflake;
 
     private Redis redis;
@@ -35,11 +50,11 @@ public class IDProperties {
     private Lettuce lettuce;
 
     /**
-     * 雪花分布式ID生成器配置
+     * 分布式ID生成器配置抽象类
      */
     @Getter
     @Setter
-    public static class Snowflake {
+    private static abstract class IDWorker {
 
         /**
          * 工作机器ID
@@ -50,77 +65,75 @@ public class IDProperties {
          * 数据中心ID
          */
         private Long datacenterId;
+    }
+
+    /**
+     * Snowflake 分布式ID生成器配置
+     */
+    public static class Snowflake extends IDWorker {
+
     }
 
     /**
      * Redis 分布式ID生成器配置
      */
-    @Getter
-    @Setter
-    public static class Redis {
+    public static class Redis extends IDWorker {
 
-        /**
-         * 工作机器ID
-         */
-        private Long workerId;
-
-        /**
-         * 数据中心ID
-         */
-        private Long datacenterId;
     }
 
     /**
      * Redisson 分布式ID生成器配置
      */
-    @Getter
-    @Setter
-    public static class Redisson {
+    public static class Redisson extends IDWorker {
 
-        /**
-         * 工作机器ID
-         */
-        private Long workerId;
-
-        /**
-         * 数据中心ID
-         */
-        private Long datacenterId;
     }
 
     /**
      * Jedis 分布式ID生成器配置
      */
-    @Getter
-    @Setter
-    public static class Jedis {
+    public static class Jedis extends IDWorker {
 
-        /**
-         * 工作机器ID
-         */
-        private Long workerId;
-
-        /**
-         * 数据中心ID
-         */
-        private Long datacenterId;
     }
 
     /**
      * Lettuce 分布式ID生成器配置
      */
-    @Getter
-    @Setter
-    public static class Lettuce {
+    public static class Lettuce extends IDWorker {
 
-        /**
-         * 工作机器ID
-         */
-        private Long workerId;
+    }
 
-        /**
-         * 数据中心ID
-         */
-        private Long datacenterId;
+    /**
+     * 初始化默认配置
+     */
+    @PostConstruct
+    public void init() {
+        initIDWorker(this::getSnowflake, this::setSnowflake, Snowflake::new);
+        initIDWorker(this::getRedis, this::setRedis, Redis::new);
+        initIDWorker(this::getRedisson, this::setRedisson, Redisson::new);
+        initIDWorker(this::getJedis, this::setJedis, Jedis::new);
+        initIDWorker(this::getLettuce, this::setLettuce, Lettuce::new);
+    }
+
+    /**
+     * 初始化分布式ID生成器默认配置
+     *
+     * @param getter      分布式ID生成器 getter 方法
+     * @param setter      分布式ID生成器 setter 方法
+     * @param constructor 分布式ID生成器构造方法
+     * @param <T>         分布式ID生成器类型
+     */
+    private <T extends IDWorker> void initIDWorker(Supplier<T> getter, Consumer<T> setter, Supplier<T> constructor) {
+        // 如果分布式ID生成器配置为空, 创建分布式ID生成器配置
+        if (Objects.isNull(getter.get())) {
+            setter.accept(constructor.get());
+        }
+        // 如果分布式ID生成器的工作机器ID为空, 使用全局工作机器ID配置
+        if (Objects.isNull(getter.get().getWorkerId())) {
+            getter.get().setWorkerId(this.workerId);
+        }
+        // 如果分布式ID生成器的数据中心ID为空, 使用全局数据中心ID配置
+        if (Objects.isNull(getter.get().getDatacenterId())) {
+            getter.get().setDatacenterId(this.datacenterId);
+        }
     }
 }
