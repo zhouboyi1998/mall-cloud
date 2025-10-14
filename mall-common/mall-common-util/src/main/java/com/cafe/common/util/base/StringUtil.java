aa4a00698297fd1663ad03e61235d74d4c4baa5b
zhouboyi <zby02126265@alibaba-inc.com>
2025-10-14 17:20:53 +0800

    docs(string): 字符串工具类添加方法注释

--- mall-common/mall-common-util/src/main/java/com/cafe/common/util/base/StringUtil.java
+++ mall-common/mall-common-util/src/main/java/com/cafe/common/util/base/StringUtil.java
@@ -9,10 +9,22 @@ package com.cafe.common.util.base;
  */
 public class StringUtil {
 
+    /**
+     * 字符串首字母转换为大写
+     *
+     * @param str 源字符串
+     * @return 目标字符串
+     */
     public static String upperCaseFirstLetter(String str) {
         return str == null || str.isEmpty() ? str : str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 
+    /**
+     * 字符串首字母转换为小写
+     *
+     * @param str 源字符串
+     * @return 目标字符串
+     */
     public static String lowerCaseFirstLetter(String str) {
         return str == null || str.isEmpty() ? str : str.substring(0, 1).toLowerCase() + str.substring(1);
     }
